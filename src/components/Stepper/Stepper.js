d42f7367308dc6e11d0c8fface4138401d71f450
Nguyễn Phan Hùng Thuận <hungthuanmk@gmail.com>
2020-08-14 00:01:24 +0700

    Update style

--- src/components/Stepper/Stepper.js
+++ src/components/Stepper/Stepper.js
@@ -6,49 +6,78 @@ import "./Stepper.scss";
 import "./Step.scss";
 
 class Step extends Component {
-    constructor(props) {
-        super(props);
-    }
+    // constructor(props) {
+    //     super(props);
+    // }
+
+    styleMap = {
+        unvisited: {
+            number: "step-number-unvisited",
+            label: "step-label-unvisited",
+        },
+        visited: { number: "step-number-visited", label: "step-label-visited" },
+        activated: {
+            number: "step-number-activated",
+            label: "step-label-activated",
+        },
+    };
+
+    getStyle = (className) => this.styleMap[this.props.state][className];
+
+    // onStepClick = (number) =>
+    //     this.props.onStepClickHandler(number);
 
     render() {
         return (
-            <div className="step" onClick={()=>console.log("Hello")}>
+            <div
+                className="step"
+                onClick={() => this.props.onStepClick(this.props._number)}
+            >
                 {/* number */}
-                <div> 
-
+                <div className={this.getStyle("number")}>
+                    {this.props._number}
                 </div>
                 {/* text */}
-                <div>
-                    {this.props.title}
-                </div>
+                <div className={this.getStyle("label")}>{this.props.title}</div>
             </div>
         );
     }
 }
 
 class Stepper extends Component {
-    // constructor(props) {
-    //     super(props);
-    // }
+    constructor(props) {
+        super(props);
+        this.props.children.map((step, idx) =>
+            React.cloneElement(step, { _number: idx + 1 })
+        );
+    }
+
+    onStepClickHandler = (stepNumber) => {
+        console.log(`Step ${stepNumber} clicked!`);
+    };
 
     render() {
         return (
             <div className="stepper">
-                {this.props.children.map((step) => (
-                    step
-                ))}
+                {/* {this.props.children} */}
+                {this.props.children.map((step, idx) =>
+                    React.cloneElement(step, {
+                        _number: idx + 1,
+                        onStepClick: this.onStepClickHandler,
+                        state: "visited",
+                    })
+                )}
             </div>
         );
     }
 
-    static getDerivedStateFromProps(props, state) {
-
-    }
+    // static getDerivedStateFromProps(props, state) {}
 }
 
 Step.propTypes = {
     title: PropTypes.node.isRequired,
-    activateKey: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+    activateKey: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+        .isRequired,
 };
 
 Stepper.propTypes = {
